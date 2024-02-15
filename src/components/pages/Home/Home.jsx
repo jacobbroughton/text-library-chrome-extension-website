3352d3000c7099157a96b9eec2fe852a353feafb
Jacob Broughton <jlbroughton88@gmail.com>
2024-02-14 21:24:56 -0500

    fixed image path

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <p>Keep a note nearby!</p>
       </div>
       <div className="image-container">
-        <img src="./screenshot.jpg" alt="screenshot of chrome extension" />
+        <img src="/screenshot.jpg" alt="screenshot of chrome extension" />
       </div>
 
       <div className="links">
